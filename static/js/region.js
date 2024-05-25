665fbea7ebcfbf0324d9c271931adf3e67ed0205
inseoks <inseok0416@gmail.com>
2024-05-26 00:18:34 +0900

    very_good

--- static/js/region.js
+++ static/js/region.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // .other_region_box에 클릭 이벤트를 추가합니다.
+    // .other_region_box에 클릭 이벤트를 추가
     document.querySelector('.other_region_box').addEventListener('click', function(event) {
-        // 클릭된 요소가 .other_region 클래스를 가지고 있는지 확인합니다.
+        // 클릭된 요소가 .other_region 클래스를 가지고 있는지 확인
         if (event.target.classList.contains('other_region')) {
-            // 모든 .other_region 요소의 배경색을 초기화합니다.
+            // 모든 .other_region 요소의 배경색을 초기화
             document.querySelectorAll('.other_region').forEach(function(item) {
                 item.style.backgroundColor = '';
             });
 
-            // 클릭된 요소의 배경색을 변경합니다.
+            // 클릭된 요소의 배경색을 변경
             event.target.style.backgroundColor = '#e4e4e5';
         }
     });
 
-    // .region_child_child_list에 클릭 이벤트를 추가합니다.
+    // .region_child_child_list에 클릭 이벤트를 추가
     document.querySelectorAll('.region_child_child_box').forEach(function(box) {
         box.addEventListener('click', function(event) {
-            // 클릭된 요소가 .region_child_child 클래스를 가지고 있는지 확인합니다.
+            // 클릭된 요소가 .region_child_child 클래스를 가지고 있는지 확인
             if (event.target.classList.contains('region_child_child')) {
-                // 모든 .region_child_child 요소의 배경색을 초기화합니다.
+                // 모든 .region_child_child 요소의 배경색을 초기화
                 box.querySelectorAll('.region_child_child').forEach(function(item) {
                     item.style.backgroundColor = '';
                 });
 
-                // 클릭된 요소의 배경색을 변경합니다.
+                // 클릭된 요소의 배경색을 변경
                 event.target.style.backgroundColor = '#e4e4e5';
 
                 document.querySelector('.title_r_r').textContent = event.target.textContent;   
